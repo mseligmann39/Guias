ef6ea8e9910dfa4eb0bdab56dcee582173e669f9
mseligmann39 <mseligmann39@gmail.com>
2025-10-11 17:29:15 +0200

    GameList funcional, falta agregar datos a la db

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+function HomePage() {
+    // estado para almacenar los juegos
+    const [games, setGames] = useState([]);
+    // useEffect para hacer la peticion a la api
+    useEffect(() => {
+
+        // url de la api
+        const apiURL = `${import.meta.env.VITE_API_BASE_URL}games`;
+        
+        // hacer la peticion get
+
+        axios.get(apiURL)
+            .then(response => setGames(response.data))
+            .catch(error => console.log(error));
+    }, []);
+    
+    // renderizar la lista de juegos
+    return (
+        console.log(games),
+        <ul>
+            {games.map(game => (
+                <li key={game.id}>{game.title}</li>
+            ))}
+        </ul>
+    );
+    
+}
+
+export default HomePage;
+
